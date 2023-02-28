ee579554e7437fd9dd4ae594e5403faf36e9d03e
luciaguerra <luciaguerra441@gmail.com>
2023-02-27 23:38:12 -0300

    Initialise repository

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let inputButton = document.getElementById("input-btn");
+let myLeads = [];
+let inputEl = document.getElementById("input-el");
+
+inputButton.addEventListener("click", function(){
+    console.log("Button clicked");
+})
+
+/*let boxEl = document.getElementById("box");
+
+boxEl.addEventListener("click", function() {
+    console.log("i want to open the box!")
+})*/
+
